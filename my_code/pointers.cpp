d9104553270db527d70d229c61e1fbcfd8a59c1a
Ellie Haber <elh391@nyu.edu>
2019-06-04 11:38:35 -0400

    lab 2

---
+++ my_code/pointers.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "pointers.h"
+
+using namespace std;
+
+void inc_by_value(int n) {
+    ++n;
+}
+
+
+void inc_with_pointer(int* iptr) {
+    (*iptr)++;
+}
+
+
+void inc_with_reference(int& n) {
+    *n++;
+}
+
+
+Thing** create_array_of_things(int n) {
+    for (int i = 0; i < n; i++) {
+        things[i] = new Thing(i);
+    }
+    return nullptr;
+}
+
+void print_all_things(Thing** things, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << things[i]->val << ' ';
+    }
+}
+
+void double_all_things(Thing** things, int n) {
+    for (int i = 0; i < n; i++) {
+        things[i] -> val *= 2;
+    }
+}
+
+void delete_all_things(Thing** things, int n) {
+    for (int i = 0; i < n; i++) {
+        delete things[i];
+    }
+    delete things[];
+}
+
+
+void assignTA(Student& s, Student& ta) {
+}
+
+void printTAs(vector<Student> students) {
+}
+
