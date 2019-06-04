c5800f63631c8d95a7c848b8a4f6f9e39b163d95
Ellie Haber <elh391@nyu.edu>
2019-06-04 12:21:36 -0400

    pointers

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -19,9 +19,8 @@ void inc_with_reference(int& n) {
 
 
 Thing** create_array_of_things(int n) {
-    Thing things [n] = {};
     for (int i = 0; i < n; i++) {
-        things[i] = new Thing(i);
+        Thing*[i] = new Thing(i);
     }
     return nullptr;
 }
