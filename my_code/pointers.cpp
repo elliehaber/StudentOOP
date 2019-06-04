17a11eb10147a681d0f57f721d71f17054d3a093
Ellie Haber <elh391@nyu.edu>
2019-06-04 13:06:37 -0400

    pointers

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -46,11 +46,11 @@ void delete_all_things(Thing** things, int n) {
 }
 
 
-void assignTA(Student& s, Student& ta) {
+void assignTA(Student* s, Student* ta) {
     s->ta= ta;
 }
 
-void printTAs(vector<Student> students) {
+void printTAs(vector<Student*> students) {
     for (Student* student : students) {
         if(student -> ta == nullptr) {
             count << student -> name << " has no ta"\n";
