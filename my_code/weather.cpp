8177697c0b80276e1152b7f6591b2d2f235b61c1
Ellie Haber <elh391@nyu.edu>
2019-06-06 12:33:16 -0400

    weather lab

---
+++ my_code/weather.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "weather.h"
+
+using namespace std;
+
+/*
+ * output for GPS coordinates
+ * */
+ostream& operator<<(std::ostream& os, const GPS& gps){
+    os << "latitude: " << gps.latitude << "; longitude: " << gps.longitude;
+    return os;
+}
+
+/*
+ * A constructor for weather class.
+ * */
+Weather::Weather(string nm, GPS loc) :
+    station_nm(nm), my_loc(loc) {
+}
+
+
+string Weather::get_name() const {
+    return station_nm;
+}
+int Weather::get_rating() const{
+    return rating;
+    
+}
+void Weather::set_rating(int new_rating){
+    int rating = new_rating;
+}
+
+ostream& operator<<(ostream& os, const Weather& w){
+    os << w.get_name() <<  ": rating " << w.get_rating() << " " << w.my_loc;
+    return os;
+}
+
+
+
